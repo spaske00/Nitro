e677ad9053f4ad2cef3899c9fb1b815976a17677
spaske00 <spaskeasm@gmail.com>
2020-04-26 16:55:31 +0200

    dumy track

--- Game/TrackController.cpp
+++ Game/TrackController.cpp
@@ -10,13 +10,13 @@ bool Nitro::TrackController::Init(Engine::Renderer* renderer_, Engine::EntityMan
 	ASSERT(entityManager_ != nullptr, "Must pass a valid entity manager");
 
 	{
-		Engine::Matrix<Engine::ColorA> colorMatrix(100, 3);
+		Engine::Matrix<Engine::ColorA> colorMatrix(100, 1);
 		
-		for (int i = 0; i < colorMatrix.Rows(); ++i)
+		for (int i = 0; i + 3 < colorMatrix.Rows(); i += 3)
 		{
 			colorMatrix.At(i, 0) = Engine::ColorA{ 255, 0, 0, 255 };
-			colorMatrix.At(i, 1) = Engine::ColorA{ 0, 255, 0, 255 };
-			colorMatrix.At(i, 2) = Engine::ColorA{ 0, 0, 255, 255 };
+			colorMatrix.At(i + 1, 0) = Engine::ColorA{ 0, 255, 0, 255 };
+			colorMatrix.At(i + 2, 0) = Engine::ColorA{ 0, 0, 255, 255 };
 		}
 		auto result = Engine::Texture::CreateMatrixOfTexturesFromMatrixOfColors(renderer_, colorMatrix, 1280, 200);
 
@@ -25,7 +25,7 @@ bool Nitro::TrackController::Init(Engine::Renderer* renderer_, Engine::EntityMan
 			for (int j = 0; j < result.Cols(); ++j)
 			{
 				auto background = Engine::Entity::Create();
-				background->AddComponent<Engine::TransformComponent>(100 + j * 200.f, -i * 1280.f, 600.f, 1280.f);
+				background->AddComponent<Engine::TransformComponent>(100 + j * 200.f, -i * 1280.f, 1280.f, 720.f * 7);
 				background->AddComponent<Engine::SpriteComponent>().m_Image = result.At(i, j).get();
 				background->AddComponent<Engine::BackgroundComponent>();
 				textureManager_->AddTexture(fmt::format("background{}{}", i, j), std::move(result.At(i, j)));
