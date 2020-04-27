a764a1f08bd1e2725efcabf6e6a15e90a774d4ae
Dara123M <44550316+Dara123M@users.noreply.github.com>
2020-04-27 11:47:08 +0200

    Add files via upload

--- Game/TrackController.cpp
+++ Game/TrackController.cpp
@@ -10,38 +10,51 @@ bool Nitro::TrackController::Init(Engine::Renderer* renderer_, Engine::EntityMan
 	ASSERT(entityManager_ != nullptr, "Must pass a valid entity manager");
 
 	{
-		Engine::Matrix<Engine::ColorA> colorMatrix(64000, 100);
+		Engine::Matrix<Engine::ColorA> colorMatrix(100, 1);
 		
-		for (int i = 0; i < colorMatrix.Rows(); ++i)
+		for (int i = 0; i + 3 < colorMatrix.Rows(); i += 3)
 		{
-			for(int j = 0; j < colorMatrix.Cols(); ++j)
-			{
-				colorMatrix.At(i, j) = Engine::ColorA{ rand() % 256u, rand() % 256u, rand() % 256u, 255 };
-
-			}
-			/*colorMatrix.At(i, 0) = Engine::ColorA{ 0, 255, 0, 255 };
-			colorMatrix.At(i, 1) = Engine::ColorA{ 192, 192, 192, 255 };
-			colorMatrix.At(i, 2) = Engine::ColorA{ 192, 192, 192, 255 };
-			colorMatrix.At(i, 3) = Engine::ColorA{ 192, 192, 192, 255 };
-			colorMatrix.At(i, 4) = Engine::ColorA{ 192, 192, 192, 255 };
-			colorMatrix.At(i, 5) = Engine::ColorA{ 0, 0, 255, 255 };*/
+			colorMatrix.At(i, 0) = Engine::ColorA{ 255, 0, 0, 255 };
+			colorMatrix.At(i + 1, 0) = Engine::ColorA{ 0, 255, 0, 255 };
+			colorMatrix.At(i + 2, 0) = Engine::ColorA{ 0, 0, 255, 255 };
 		}
-		
-		auto result = Engine::Texture::CreateMatrixOfTexturesFromMatrixOfColors(renderer_, colorMatrix, 8, 8);
-
+		auto result = Engine::Texture::CreateMatrixOfTexturesFromMatrixOfColors(renderer_, colorMatrix, 1280, 200);
+		if (!textureManager_->CreateTexture(renderer_, "grass", "Resource/g1.jpg"))
+		{
+			LOG_ERROR("Failed to create backgruondCoordinate");
+			return false;
+		}
+		if (!textureManager_->CreateTexture(renderer_, "road", "Resource/road.png"))
+		{
+			LOG_ERROR("Failed to create backgruondCoordinate");
+			return false;
+		}
 		for(int i = 0; i < result.Rows(); ++i)
 		{
 			for (int j = 0; j < result.Cols(); ++j)
 			{
 				auto background = Engine::Entity::Create();
-				auto dimensions = result.At(i, j).get()->QueryDimensions();
-				background->AddComponent<Engine::TransformComponent>((float)dimensions.width * j, -(float)dimensions.height * i, (float)dimensions.width, (float)dimensions.height);
-				background->AddComponent<Engine::SpriteComponent>().m_Image = result.At(i, j).get();
+				background->AddComponent<Engine::TransformComponent>(100 + j * 200.f, -i * 1280.f, 1280.f, 720.f * 7);
+				background->AddComponent<Engine::SpriteComponent>().m_Image = textureManager_->GetTexture("grass");
+				;//result.At(i, j).get();
 				background->AddComponent<Engine::BackgroundComponent>();
-				textureManager_->AddTexture(fmt::format("background{}{}", i, j), std::move(result.At(i, j)));
+				//textureManager_->AddTexture(fmt::format("background{}{}", i, j), std::move(result.At(i, j)));
+				auto background1 = Engine::Entity::Create();
+				background1->AddComponent<Engine::TransformComponent>(100 + j * 200.f, -i * 1280.f, 400.f, 720.f * 7);
+				background1->AddComponent<Engine::SpriteComponent>().m_Image = textureManager_->GetTexture("road");
+				;//result.At(i, j).get();
+				background1->AddComponent<Engine::BackgroundComponent>();
+				//textureManager_->AddTexture(fmt::format("background{}{}", i, j), std::move(result.At(i, j)));
+
+				
 				entityManager_->AddEntity(std::move(background));
+				entityManager_->AddEntity(std::move(background1));
 			}
 		}
+
+
+		LOG_INFO("Size of matrix {} {}", result.Rows(), result.Cols());
+		
 	}
 	
 	
